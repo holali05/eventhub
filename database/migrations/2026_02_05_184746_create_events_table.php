e69e8a9dae37a4981faa17b6d82ded3b4bab7d84
Holali <holalitohouegnon@gmail.com>
2026-02-05 19:54:57 +0100

    Mise à jour du fichier de la table de création d'évènements

--- database/migrations/2026_02_05_184746_create_events_table.php
+++ database/migrations/2026_02_05_184746_create_events_table.php
@@ -13,6 +13,16 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            // Lien avec l'organisateur (si l'utilisateur est supprimé, ses événements aussi)
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('location');
+            $table->date('event_date');
+            $table->time('event_time');
+            $table->string('image_path')->nullable(); // Pour l'affiche de l'événement
+            
             $table->timestamps();
         });
     }
