4ec6d9b2b987f1204752c43bf72ba1f04a51ad49
Holali <holalitohouegnon@gmail.com>
2026-02-05 20:36:42 +0100

    Enregistrement du middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\CheckApproved; // Import du middleware
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Enregistrement de l'alias du middleware
+        $middleware->alias([
+            'approved' => CheckApproved::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
