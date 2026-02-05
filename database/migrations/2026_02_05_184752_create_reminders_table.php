1f0932a42ff1b77edf0206c73e96f528e4671707
Holali <holalitohouegnon@gmail.com>
2026-02-05 20:17:11 +0100

    Mise à jour de la table de programmations des rappels automatiques

--- database/migrations/2026_02_05_184752_create_reminders_table.php
+++ database/migrations/2026_02_05_184752_create_reminders_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('reminders', function (Blueprint $table) {
             $table->id();
+            // Lien avec l'événement
+            $table->foreignId('event_id')->constrained()->onDelete('cascade');
+            
+            $table->text('message'); // Le message personnalisé
+            $table->string('frequency'); // 'daily', 'weekly', 'monthly'
+            
+            $table->timestamp('last_sent_at')->nullable(); // Utile pour que le script sache quand il a tourné pour la dernière fois
+            $table->boolean('is_active')->default(true); // Pour permettre à l'organisateur de stopper les relances
+            
             $table->timestamps();
         });
     }
